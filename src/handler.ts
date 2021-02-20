ea3ba0089565d187cfef3660379efcfd6e553f1e
Drew Wyatt <drew.j.wyatt@gmail.com>
2021-02-20 13:51:03 -0500

    log traffic

--- src/handler.ts
+++ src/handler.ts
@@ -1,8 +1,23 @@
 import type { APIGatewayProxyHandler } from 'aws-lambda'
 
 export const handler: APIGatewayProxyHandler = async event => {
+  const eventDetails = JSON.stringify(
+    {
+      body: event.body,
+      headers: event.headers,
+      method: event.httpMethod,
+      path: event.path,
+      pathParameters: event.pathParameters,
+      queryString: event.queryStringParameters,
+    },
+    null,
+    2,
+  )
+
+  console.log(eventDetails)
+
   return {
-    body: JSON.stringify({ wow: 'neat' }),
+    body: eventDetails,
     headers: {
       'Content-Type': 'application/json',
     },
